662cd09c37b5c72d7a92c4929c5d8a6f323281d8
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2025-06-08 18:07:38 -0300

    external packages

--- 1-packages/main.go
+++ 1-packages/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"modulo/assistant"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Write from file main.go")
 	assistant.Write()
+
+	error:= checkmail.ValidateFormat("teste@gmail.com")
+	fmt.Println("Email validation result:", error)
+	fmt.Println("Email validation result:", checkmail.ValidateFormat("teste@"))
 }
